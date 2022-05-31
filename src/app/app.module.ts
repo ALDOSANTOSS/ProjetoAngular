9598c07bfcc98ea1855f5b7b49fc79aaf9ce90ea
Calculadora <aldo.santos.oliveira95@gmail.com>
2022-05-31 03:02:51 -0300

    Iniciando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrimeiroComponenteComponet } from './primeiro-componente.componet';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponenteComponet
+
   ],
   imports: [
     BrowserModule
